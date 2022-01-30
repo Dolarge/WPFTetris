4894ba2b1215e60156a62bf9b5c87afefdcb1cc6
Dolarge <dohyun722@daum.net>
2022-01-30 12:21:28 +0900

    feature : 2DArrayofViewModel

--- Tetris/ViewModel/2DArrayViewModel.cs
+++ Tetris/ViewModel/2DArrayViewModel.cs
@@ -1,12 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace Tetris.ViewModel
 {
-    class _2DArrayViewModel
+
+    public class Arrayof2DVIewmodel<T> : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Notify(string property)
+        {
+            var pc = PropertyChanged;
+            if (pc != null)
+                pc(this, new PropertyChangedEventArgs(property));
+        }
+
+        T[,] data;
+
+        public void NotifyBlockChange()
+        {
+            Notify(Binding.IndexerName);
+        }
+
+        public T this[int c1, int c2]
+        {
+            get { return data[c1, c2]; }
+            set
+            {
+                data[c1, c2] = value;
+                //Notify(Binding.IndexerName);
+            }
+        }
+
+        public string GetStringIndex(int c1, int c2)
+        {
+            return c1.ToString() + "-" + c2.ToString();
+        }
+
+        private void SplitIndex(string index, out int c1, out int c2)
+        {
+            var parts = index.Split('-');
+            if (parts.Length != 2)
+                throw new ArgumentException("The provided index is not valid");
+
+            c1 = int.Parse(parts[0]);
+            c2 = int.Parse(parts[1]);
+        }
+
+        public T this[string index]
+        {
+            get
+            {
+                int c1, c2;
+                SplitIndex(index, out c1, out c2);
+                return data[c1, c2];
+            }
+            set
+            {
+                int c1, c2;
+                SplitIndex(index, out c1, out c2);
+                data[c1, c2] = value;
+                //Notify(Binding.IndexerName);
+            }
+        }
+
+        public Arrayof2DVIewmodel(int size1, int size2)
+        {
+            data = new T[size1, size2];
+        }
+
+        public static implicit operator T[,](Arrayof2DVIewmodel<T> a)
+        {
+            return a.data;
+        }
     }
 }
